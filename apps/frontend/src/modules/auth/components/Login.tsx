6fd7d148cccff16123f6ba480e2675b3f3a45a93
afolasope <folasopeadebanjo@gmail.com>
2023-11-06 18:18:49 +0100

    fix login

--- apps/frontend/src/modules/auth/components/Login.tsx
+++ apps/frontend/src/modules/auth/components/Login.tsx
@@ -1,122 +1,123 @@
 import { GlobalSpinner } from "@/components/GlobalSpinner";
 import { useGoogleUserAuthMutation } from "@/generated/graphql";
+import { useAuthToken } from "@/modules/auth/hooks/useAuthToken";
 import { Button, TextInput } from "@/ui";
 import { Google } from "@/ui/icons";
 import { useGoogleLogin } from "@react-oauth/google";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { handleRedirect } from "../helper";
-import { useLogin } from "../hooks.internal/useLogin";
 
 function Login({ variant = "login" }: { variant?: "login" | "signup" }) {
-  const [email, setEmail] = useState("");
-  const navigate = useNavigate();
-  const login = useLogin();
+    const [email, setEmail] = useState("");
+    const navigate = useNavigate();
+    const { login } = useAuthToken();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    };
 
-  const [googleAuth, { loading }] = useGoogleUserAuthMutation();
+    const [googleAuth, { loading }] = useGoogleUserAuthMutation();
 
-  const loginWithGoogle = useGoogleLogin({
-    flow: "auth-code",
-    ux_mode: "popup",
-    onSuccess: async (codeResponse) => {
-      const result = await googleAuth({
-        variables: {
-          code: codeResponse.code,
+    const loginWithGoogle = useGoogleLogin({
+        flow: "auth-code",
+        ux_mode: "popup",
+        onSuccess: async (codeResponse) => {
+            const result = await googleAuth({
+                variables: {
+                    code: codeResponse.code,
+                },
+            });
+            if (result.data?.googleUserAuth) {
+                if (
+                    !result.data?.googleUserAuth?.token ||
+                    !result.data?.googleUserAuth?.refreshToken ||
+                    !result.data?.googleUserAuth?.csrfToken
+                )
+                    return;
+
+                if (result.data?.googleUserAuth?.user) {
+                    handleRedirect(result.data?.googleUserAuth?.user, navigate);
+                }
+
+                login(
+                    result.data?.googleUserAuth?.token,
+                    result.data?.googleUserAuth?.refreshToken,
+                    result.data?.googleUserAuth?.csrfToken,
+                );
+            }
         },
-      });
-      if (result.data?.googleUserAuth) {
-        if (
-          !result.data?.googleUserAuth?.token ||
-          !result.data?.googleUserAuth?.refreshToken ||
-          !result.data?.googleUserAuth?.csrfToken
-        )
-          return;
+        onError: async () => {
+            navigate("/");
+        },
+    });
 
-        if (result.data?.googleUserAuth?.user) {
-          handleRedirect(result.data?.googleUserAuth?.user, navigate);
-        }
+    if (loading) {
+        return <GlobalSpinner />;
+    }
 
-        login(
-          result.data?.googleUserAuth?.token,
-          result.data?.googleUserAuth?.refreshToken,
-          result.data?.googleUserAuth?.csrfToken,
-        );
-      }
-    },
-    onError: async () => {
-      navigate("/");
-    },
-  });
+    return (
+        <>
+            <div className="flex w-[478px] flex-col gap-6 self-center p-12">
+                <header className="flex flex-col gap-2 text-center">
+                    <h1 className="text-[28px] font-medium leading-normal text-white">
+                        {variant === "signup"
+                            ? "Create your Integraflow account"
+                            : "Log in to Integraflow"}
+                    </h1>
+                    <p className="text-base text-intg-text">
+                        {variant === "signup"
+                            ? "Let's get your account set up"
+                            : "Welcome back 🥰"}
+                    </p>
+                </header>
 
-  if (loading) {
-    return <GlobalSpinner />;
-  }
+                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                    <TextInput
+                        placeholder="Enter your email"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Button text="Continue with Email" />
+                </form>
 
-  return (
-    <>
-      <div className="flex w-[478px] flex-col gap-6 self-center p-12">
-        <header className="flex flex-col gap-2 text-center">
-          <h1 className="text-[28px] font-medium leading-normal text-white">
-            {variant === "signup"
-              ? "Create your Integraflow account"
-              : "Log in to Integraflow"}
-          </h1>
-          <p className="text-base text-intg-text">
-            {variant === "signup"
-              ? "Let's get your account set up"
-              : "Welcome back 🥰"}
-          </p>
-        </header>
+                <div className="self-center text-base text-intg-text-6">
+                    <span>
+                        {variant === "signup"
+                            ? "Already have an account? "
+                            : "Don't have an account yet? "}
+                    </span>
+                    <Link
+                        to={variant === "signup" ? "/" : "/signup"}
+                        className="bg-gradient-button-hover bg-clip-text font-medium text-transparent"
+                    >
+                        {variant === "signup" ? "Log in" : "Sign up"}
+                    </Link>
+                </div>
 
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <TextInput
-            placeholder="Enter your email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Button text="Continue with Email" />
-        </form>
+                <hr className="border border-intg-bg-4" />
 
-        <div className="self-center text-base text-intg-text-6">
-          <span>
-            {variant === "signup"
-              ? "Already have an account? "
-              : "Don't have an account yet? "}
-          </span>
-          <Link
-            to={variant === "signup" ? "/" : "/signup"}
-            className="bg-gradient-button-hover bg-clip-text font-medium text-transparent"
-          >
-            {variant === "signup" ? "Log in" : "Sign up"}
-          </Link>
-        </div>
+                <Button
+                    variant="secondary"
+                    className="flex items-center justify-center gap-2"
+                    onClick={() => loginWithGoogle()}
+                >
+                    <Google />
+                    Continue with Google
+                </Button>
+            </div>
 
-        <hr className="border border-intg-bg-4" />
-
-        <Button
-          variant="secondary"
-          className="flex items-center justify-center gap-2"
-          onClick={() => loginWithGoogle()}
-        >
-          <Google />
-          Continue with Google
-        </Button>
-      </div>
-
-      {variant === "signup" ? (
-        <div className="max-w-xs self-center text-center text-base text-intg-text">
-          By signing up, you agree to Integraflow Privacy and terms services
-        </div>
-      ) : (
-        <div />
-      )}
-    </>
-  );
+            {variant === "signup" ? (
+                <div className="max-w-xs self-center text-center text-base text-intg-text">
+                    By signing up, you agree to Integraflow Privacy and terms
+                    services
+                </div>
+            ) : (
+                <div />
+            )}
+        </>
+    );
 }
 
 export default Login;
