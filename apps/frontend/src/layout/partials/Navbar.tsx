05fd2f4256d1045101ad88dcb31ebee0e5537428
afolasope <folasopeadebanjo@gmail.com>
2023-11-09 13:07:30 +0100

    fix navbar spacing

--- apps/frontend/src/layout/partials/Navbar.tsx
+++ apps/frontend/src/layout/partials/Navbar.tsx
@@ -8,9 +8,10 @@ import {
     QuestionIcon,
     SpeakerIcon,
 } from "@/assets/images";
+import { Button } from "@/ui";
 import { ChevronDown } from "lucide-react";
-import Discord from "../assets/images/navbar/Discord.png";
-import Frame from "../assets/images/navbar/Frame.png";
+import Discord from "../../assets/images/navbar/Discord.png";
+import Frame from "../../assets/images/navbar/Frame.png";
 
 export const Navbar = () => {
     const navItems = [
@@ -56,7 +57,7 @@ export const Navbar = () => {
 
     return (
         <div
-            className="w-[240px] border-r border-intg-bg-4 bg-intg-black p-6 pb-[31px]"
+            className="w-[240px] border-r border-intg-bg-4 bg-intg-black p-6"
             style={{
                 backgroundImage:
                     "radial-gradient(rgba(28, 15, 89, 0.30) 50%, rgba(5, 5, 5, 0.30))",
@@ -78,13 +79,13 @@ export const Navbar = () => {
                 </p>
             </div>
             <div className="mb-[14px] space-y-[27px]">
-                <button className="bg-intg-bg-9 flex items-center justify-between space-x-2 rounded border border-intg-bg-4 p-3 text-sm text-intg-text-4">
+                <button className="flex items-center justify-between space-x-2 rounded border border-intg-bg-4 bg-intg-bg-9 p-3 text-sm text-intg-text-4">
                     <span>
                         <DocumentIcon />
                     </span>
                     <span>Create new survey</span>
                 </button>
-                <button className="bg-intg-bg-8 flex  w-[177px]  items-center space-x-2 rounded px-3 py-2 text-sm text-intg-text-4">
+                <button className="flex w-[177px]  items-center  space-x-2 rounded bg-intg-bg-8 px-3 py-2 text-sm text-intg-text-4">
                     <span>
                         <CircleIcon />
                     </span>
@@ -94,7 +95,7 @@ export const Navbar = () => {
 
             <hr className="border-intg-bg-4" />
 
-            <ul className="mb-24 mt-4 space-y-2 text-sm text-intg-text-4">
+            <ul className="my-4 space-y-2 text-sm text-intg-text-4">
                 {navItems.map((item) => {
                     return (
                         <li className="flex items-center space-x-2 px-3 py-2">
@@ -105,7 +106,7 @@ export const Navbar = () => {
                 })}
             </ul>
             <hr className="border-intg-bg-4" />
-            <ul className="mb-[72px] mt-4 space-y-2 text-sm text-intg-text-4">
+            <ul className="my-4 space-y-2 text-sm text-intg-text-4">
                 {bottomNav.map((item) => {
                     return (
                         <li className="flex items-center space-x-2 px-3 py-2">
@@ -115,24 +116,26 @@ export const Navbar = () => {
                     );
                 })}
             </ul>
-            <div className="bg-intg-bg-9 mb-[54px] flex w-full space-x-2 rounded-lg px-2 py-4">
+            <div className="flex w-full space-x-2 rounded-lg bg-intg-bg-9 px-2 py-4">
                 <div className="h-8 w-8">
                     <img src={Discord} alt="Discord" />
                 </div>
-                <div className="space-y-[2px]">
+                <div className="space-y-[4px]">
                     <p className="flex">
                         <span className="text-sm text-white">
                             Join our Discord Community
                         </span>
                     </p>
-                    <p className="text-sm text-intg-text">
+                    <p className="text-xs text-intg-text">
                         Add your card to prevent interruption after trial
                     </p>
-                    {/* <Button text="Join now" /> */}
+                    <Button text="Join now" className="py-[6px]" />
                 </div>
             </div>
-            <hr className="border-intg-bg-4" />
-            <div className="mt-[18px] flex items-center text-intg-text">
+            <div className="my-4">
+                <hr className="border-intg-bg-4" />
+            </div>
+            <div className="flex items-center text-intg-text">
                 <div className="flex space-x-3">
                     <img src={Frame} alt="picture frame" />
                     <span>Profile</span>
