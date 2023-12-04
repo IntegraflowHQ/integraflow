103acc1dd465e55a743551f3ec54f607d8b4fd8f
Afeez lawal <45849676+AfeezGL@users.noreply.github.com>
2023-12-04 14:28:46 +0100

    Update DropdownMenu.tsx
    remove comments

--- apps/frontend/src/ui/Dropdown/DropdownMenu.tsx
+++ apps/frontend/src/ui/Dropdown/DropdownMenu.tsx
@@ -4,11 +4,6 @@ import { forwardRef } from "react";
 export const DropdownMenu = DropdownMenuPrimitive.Root;
 export const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
 
-// interface Props extends DropdownMenuPrimitive.DropdownMenuContentProps = {
-//     children: React.ReactNode;
-//     classname: string;
-// };
-
 export const DropdownMenuContent = forwardRef<
     HTMLDivElement,
     DropdownMenuPrimitive.DropdownMenuContentProps
@@ -30,35 +25,5 @@ export const DropdownMenuGroup = DropdownMenuPrimitive.Group;
 export const DropdownMenuSub = DropdownMenuPrimitive.DropdownMenuSub;
 export const DropdownMenuSubContent = DropdownMenuPrimitive.DropdownMenuSubContent;
 export const DropdownMenuSubTrigger = DropdownMenuPrimitive.DropdownMenuSubTrigger;
-
-
-// export const DropdownMenuCheckboxItem = React.forwardRef(
-//     ({ children, ...props }, forwardedRef) => {
-//         return (
-//             <DropdownMenuPrimitive.CheckboxItem {...props} ref={forwardedRef}>
-//                 {children}
-//                 <DropdownMenuPrimitive.ItemIndicator>
-//                     {props.checked === "indeterminate" && <PenLineIcon />}
-//                     {props.checked === true && <CheckIcon />}
-//                 </DropdownMenuPrimitive.ItemIndicator>
-//             </DropdownMenuPrimitive.CheckboxItem>
-//         );
-//     },
-// );
-
-// export const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup;
-
-// export const DropdownMenuRadioItem = React.forwardRef(
-//     ({ children, ...props }, forwardedRef) => {
-//         return (
-//             <DropdownMenuPrimitive.RadioItem {...props} ref={forwardedRef}>
-//                 {children}
-//                 <DropdownMenuPrimitive.ItemIndicator>
-//                     <CheckIcon />
-//                 </DropdownMenuPrimitive.ItemIndicator>
-//             </DropdownMenuPrimitive.RadioItem>
-//         );
-//     },
-// );
 
 export const DropdownMenuSeparator = DropdownMenuPrimitive.Separator;
