160d9e16175a6b1551e4440d66d1437e7da6957a
kaf-lamed-beyt <belac335@gmail.com>
2023-12-08 17:41:01 +0100

    feat(ui): add active state to sidebar icons

--- apps/frontend/src/modules/surveys/components/studio/create/editor-panel/index.tsx
+++ apps/frontend/src/modules/surveys/components/studio/create/editor-panel/index.tsx
@@ -1,49 +1,66 @@
 import { HelpCircle, SettingsIcon } from "@/ui/icons";
-
 import * as Tabs from "@radix-ui/react-tabs";
 import { Pen } from "lucide-react";
+import React from "react";
 import { Preview } from "../preview-panel/index.tsx";
 
 const tabs = [
-    {
-        label: "Update questions",
-        icon: <HelpCircle />,
-        content: <div>Update question</div>,
-    },
-    {
-        label: "Update design",
-        icon: <Pen size={20} color="#AFAAC7" fill="#AFAAC7" />,
-        content: <div>Design</div>,
-    },
-    {
-        label: "Update settings",
-        icon: <SettingsIcon />,
-        content: <div>Settings</div>,
-    },
+  {
+    id: crypto.randomUUID(),
+    label: "Update questions",
+    icon: <HelpCircle />,
+    content: <div>Update question</div>,
+  },
+  {
+    id: crypto.randomUUID(),
+    label: "Update design",
+    icon: <Pen size={20} color="#AFAAC7" fill="#AFAAC7" />,
+    content: <div>Design</div>,
+  },
+  {
+    id: crypto.randomUUID(),
+    label: "Update settings",
+    icon: <SettingsIcon />,
+    content: <div>Settings</div>,
+  },
 ];
 
 export default function Create() {
-    return (
-        <Tabs.Root
-            className="flex h-screen pt-[84px]"
-            defaultValue={tabs[0].label}
-        >
-            <Tabs.List className="flex h-full flex-col gap-6 border-r border-intg-bg-4 px-[18px] pt-12">
-                {tabs.map((tab) => (
-                    <Tabs.Trigger value={tab.label} className="p-2">
-                        {tab.icon}
-                    </Tabs.Trigger>
-                ))}
-            </Tabs.List>
+  const [activeTab, setActiveTab] = React.useState<string>("");
 
-            <div className="flex flex-1 gap-[38px] overflow-y-scroll pb-8 pl-5 pr-12 pt-6">
-                {tabs.map(({ content, label }) => (
-                    <Tabs.Content value={label} className="w-[519px]">
-                        {content}
-                    </Tabs.Content>
-                ))}
-                <Preview />
-            </div>
-        </Tabs.Root>
-    );
+  const handleActiveTab = (index: number) => {
+    const availableTabIds = tabs?.map(({ id }) => id);
+
+    const selectedTab = availableTabIds[index];
+    setActiveTab(selectedTab);
+
+    console.log(selectedTab);
+  };
+
+  return (
+    <Tabs.Root className="flex h-screen pt-[84px]" defaultValue={tabs[0].label}>
+      <Tabs.List className="flex h-full flex-col gap-6 border-r border-intg-bg-4 px-[18px] pt-12">
+        {tabs.map(({ id, label, icon }, index: number) => (
+          <Tabs.Trigger
+            value={label}
+            onClick={() => handleActiveTab(index)}
+            className={`hover:bg-intg-bg-13 delay-50 rounded p-2 ease-in-out  hover:transition-all ${
+              activeTab === id ? "bg-intg-bg-13" : ""
+            }`}
+          >
+            {icon}
+          </Tabs.Trigger>
+        ))}
+      </Tabs.List>
+
+      <div className="flex flex-1 gap-[38px] overflow-y-scroll pb-8 pl-5 pr-12 pt-6">
+        {tabs.map(({ content, label }) => (
+          <Tabs.Content value={label} className="w-[519px]">
+            {content}
+          </Tabs.Content>
+        ))}
+        <Preview />
+      </div>
+    </Tabs.Root>
+  );
 }
