4e0916c9597511d040f622a570e60686a8c4234f
afolasope <folasopeadebanjo@gmail.com>
2023-12-08 14:26:15 +0100

    add editor panel

---
+++ apps/frontend/src/modules/surveys/components/studio/create/editor-panel/index.tsx
@@ -0,0 +1,45 @@
+import { HelpCircle, SettingsIcon } from "@/ui/icons";
+
+import * as Tabs from "@radix-ui/react-tabs";
+import { Pen } from "lucide-react";
+
+const tabs = [
+    {
+        label: "Update questions",
+        icon: <HelpCircle />,
+        content: <div>Editor Panel</div>,
+    },
+    {
+        label: "Update design",
+        icon: <Pen size={20} color="#AFAAC7" fill="#AFAAC7" />,
+        content: <div>Design</div>,
+    },
+    {
+        label: "Update settings",
+        icon: <SettingsIcon />,
+        content: <div>Settings</div>,
+    },
+];
+
+export default function Create() {
+    return (
+        <Tabs.Root
+            className="flex h-screen pt-[84px]"
+            defaultValue={tabs[0].label}
+        >
+            <Tabs.List className="flex h-full flex-col gap-6 border-r border-intg-bg-4 px-[18px] pt-12">
+                {tabs.map((tab) => (
+                    <Tabs.Trigger value={tab.label} className="p-2">
+                        {tab.icon}
+                    </Tabs.Trigger>
+                ))}
+            </Tabs.List>
+
+            {tabs.map(({ content, label }) => (
+                <Tabs.Content value={label} className="flex-1" asChild>
+                    {content}
+                </Tabs.Content>
+            ))}
+        </Tabs.Root>
+    );
+}
---
+++ apps/frontend/src/modules/surveys/components/studio/create/preview-panel/index.tsx
@@ -0,0 +1,18 @@
+import { Header } from "@/ui";
+import EmptySurvey from "assets/images/surveys/empty.png";
+
+export const Preview = () => {
+    return (
+        <div className="flex min-w-[580px] flex-1 flex-col items-center justify-center rounded-xl bg-intg-bg-9">
+            {/* Empty state */}
+            <div className="space-y-8">
+                <img src={EmptySurvey} alt="" />
+                <Header
+                    title="Nothing to see here yet."
+                    description="Add your first question first!"
+                    className="text-center"
+                />
+            </div>
+        </div>
+    );
+};
