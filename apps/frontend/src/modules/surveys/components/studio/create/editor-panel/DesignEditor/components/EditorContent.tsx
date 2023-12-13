e75d47f8c2dc508776b6559f7bcb3e4a77494c2a
kaf-lamed-beyt <belac335@gmail.com>
2023-12-13 01:40:25 +0100

    refactor

--- apps/frontend/src/modules/surveys/components/studio/create/editor-panel/DesignEditor/components/EditorContent.tsx
+++ apps/frontend/src/modules/surveys/components/studio/create/editor-panel/DesignEditor/components/EditorContent.tsx
@@ -1,6 +1,9 @@
 import { useThemes } from "@/modules/projects/hooks/useTheme";
+import { Button } from "@/ui";
 import React from "react";
 import toast from "react-hot-toast";
+import { EditorSpinner } from "./EditorSpinner";
+import { Error } from "./Errors";
 import { ThemeCard } from "./ThemeCard";
 interface ContentProp {
     onOpen: () => void;
@@ -46,7 +49,7 @@ export const DesignEditorContent = ({ onOpen }: ContentProp) => {
     const selectedThemeIndex = data?.findIndex(
         (theme) => theme.id === selectedTheme,
     );
-    const selectedThemeData = data?.[selectedThemeIndex];
+    const selectedThemeData = data?.[selectedThemeIndex ?? 0];
 
     const onSlectedTheme = (index: number) => {
         const selectedTheme = data?.[index]?.id || "";
@@ -93,192 +96,131 @@ export const DesignEditorContent = ({ onOpen }: ContentProp) => {
     const { colors } = currentTheme;
 
     const allThemes = getCustomThemes().map((theme) => theme);
-
     if (error) toast.error(error.message);
 
     return (
         <>
-            {/* selected theme */}
-            {selectedTheme ? (
-                <div className="flex gap-5">
-                    <div className="flex py-2">
-                        {Array.isArray(colors)
-                            ? colors.map((color) => {
-                                  return (
-                                      <div
-                                          className={`h-8 w-8 rounded-full border-2 ${
-                                              color !== 0 ? "-ml-4" : ""
-                                          }`}
-                                          key={color}
-                                          style={{
-                                              backgroundColor: `${color}`,
-                                          }}
-                                      />
-                                  );
-                              })
-                            : null}
-                    </div>
+            {totalCount === 0 ? (
+                <>
+                    <Error message="You don't have any theme. Click the button below to create one." />
+                </>
+            ) : error?.networkError ? (
+                <Error
+                    message={
+                        error?.networkError.message ||
+                        "An error occured while loading themes. Please check your internet connect and try again"
+                    }
+                />
+            ) : error ? (
+                <Error
+                    message={
+                        error.message
+                            ? error.message
+                            : "An error occured, try refreshing your browser. If the issue persists, contact support."
+                    }
+                />
+            ) : !loading ? (
+                <>
+                    {selectedTheme ? (
+                        <>
+                            <p className="py-4 text-sm font-normal uppercase">
+                                selected theme
+                            </p>
+                            <div className="flex w-full gap-5 rounded-md bg-intg-bg-15 px-3 py-2 transition-all ease-in-out">
+                                <div className="flex gap-5">
+                                    <div className="flex py-2">
+                                        {Array.isArray(colors)
+                                            ? colors.map((color, index) => {
+                                                  return (
+                                                      <div
+                                                          className={`h-8 w-8 rounded-full border-2 ${
+                                                              index !== 0
+                                                                  ? "-ml-4"
+                                                                  : ""
+                                                          }`}
+                                                          key={color}
+                                                          style={{
+                                                              backgroundColor: `${color}`,
+                                                          }}
+                                                      />
+                                                  );
+                                              })
+                                            : null}
+                                    </div>
 
-                    <div>
-                        <p className="font-normal leading-6 first-letter:capitalize">
-                            {Array.isArray(currentTheme)
-                                ? null
-                                : currentTheme.name}
+                                    <div>
+                                        <p className="font-normal leading-6 first-letter:capitalize">
+                                            {Array.isArray(currentTheme)
+                                                ? null
+                                                : currentTheme.name}
+                                        </p>
+                                        <p className="font-normal text-intg-text-4">
+                                            Fetched theme
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    ) : (
+                        <p className="py-4 text-sm font-normal text-intg-text-4 first-letter:capitalize">
+                            You have not selected a theme yet
                         </p>
-                        <p className="font-normal text-intg-text-4">
-                            Fetched theme
-                        </p>
-                    </div>
-                </div>
-            ) : null}
+                    )}
 
-            {/* all themes */}
-            <div className="flex-col">
-                {allThemes?.map((theme, index: number) => {
-                    return (
-                        <div key={theme.id}>
-                            <ThemeCard
-                                activeTheme={theme.id === selectedTheme}
-                                name={theme.name}
-                                colors={theme.colors}
-                                onClick={() => onSlectedTheme(index)}
-                                toggleNewThemeModal={onOpen}
+                    {totalCount === 0 ? (
+                        <Button
+                            text="new theme"
+                            onClick={onOpen}
+                            variant="secondary"
+                            className="mb-2 mt-4 text-sm font-normal first-letter:capitalize"
+                        />
+                    ) : (
+                        <div
+                            className={`h-full py-6 ${
+                                totalCount !== 0 ? "-mt-4" : ""
+                            } transition-all delay-100 duration-300 ease-in-out`}
+                        >
+                            <p className="py-2 text-sm font-normal capitalize">
+                                all themes
+                            </p>
+
+                            <Button
+                                text="new theme"
+                                onClick={onOpen}
+                                variant="secondary"
+                                className="text-sm font-normal first-letter:capitalize"
                             />
+
+                            {/* all themes */}
+                            <div className="flex-col">
+                                {allThemes?.map((theme, index: number) => {
+                                    return (
+                                        <div key={theme.id}>
+                                            <ThemeCard
+                                                activeTheme={
+                                                    theme.id === selectedTheme
+                                                }
+                                                name={theme.name}
+                                                colors={theme.colors}
+                                                onClick={() =>
+                                                    onSlectedTheme(index)
+                                                }
+                                                toggleNewThemeModal={onOpen}
+                                            />
+                                        </div>
+                                    );
+                                })}
+                            </div>
                         </div>
-                    );
-                })}
-            </div>
+                    )}
+                </>
+            ) : (
+                <EditorSpinner
+                    startColor="#53389E"
+                    endColor="#d9d9d9"
+                    size="10"
+                />
+            )}
         </>
     );
-
-    // return (
-    //     <div>
-    //         {totalCount === 0 ? (
-    //             <Error message="You don't have any theme. Click the button below to create one." />
-    //         ) : error?.networkError ? (
-    //             <Error
-    //                 message={
-    //                     error?.networkError?.message ||
-    //                     "An error occurred while loading themes. Please check your internet connection and try again."
-    //                 }
-    //             />
-    //         ) : error ? (
-    //             <Error
-    //                 message={
-    //                     error?.message
-    //                         ? error.message
-    //                         : "An error occured, try refreshng your browser. If the error still persists, contact support."
-    //                 }
-    //             />
-    //         ) : (
-    //             <>
-    //                 {!loading ? (
-    //                     <>
-    //                         <p className="py-4 text-sm font-normal uppercase">
-    //                             Selected Theme
-    //                         </p>
-
-    //                         <div
-    //                             className={`trans flex w-full gap-5 rounded-md bg-intg-bg-15 px-3 py-2 transition-all ease-in-out`}
-    //                             onClick={() =>
-    //                                 onSelectedTheme(
-    //                                     selectedThemeIndex as number,
-    //                                 )
-    //                             }
-    //                         >
-    //                             <div className="flex py-2">
-    //                                 {palettes
-    //                                     ?.find(
-    //                                         (_, index) =>
-    //                                             index === selectedThemeIndex,
-    //                                     )
-    //                                     ?.map((color, colorIndex) => (
-    //                                         <div
-    //                                             className={`h-8 w-8 rounded-full border-2  ${
-    //                                                 colorIndex !== 0
-    //                                                     ? "-ml-4"
-    //                                                     : ""
-    //                                             }`}
-    //                                             key={colorIndex}
-    //                                             style={{
-    //                                                 backgroundColor: color,
-    //                                             }}
-    //                                         />
-    //                                     ))}
-    //                             </div>
-
-    //                             <div>
-    //                                 <p className="text-base font-normal leading-6">
-    //                                     {themeName?.find(
-    //                                         (_, index) =>
-    //                                             index === selectedThemeIndex,
-    //                                     )}
-    //                                 </p>
-    //                                 <p className="text-sm font-normal text-intg-text-4">
-    //                                     Fetched theme
-    //                                 </p>
-    //                             </div>
-    //                         </div>
-
-    //                         {totalCount === 0 ? (
-    //                             <Button
-    //                                 text="new theme"
-    //                                 onClick={onOpen}
-    //                                 variant="secondary"
-    //                                 className="mb-2 mt-4 text-sm font-normal first-letter:capitalize"
-    //                             />
-    //                         ) : (
-    //                             <div
-    //                                 className={`h-full py-6  ${
-    //                                     totalCount === 2 ? "-mt-4" : ""
-    //                                 } transition-all delay-300 ease-in-out`}
-    //                             >
-    //                                 <p className="py-2 text-sm font-normal capitalize">
-    //                                     all themes
-    //                                 </p>
-
-    //                                 <Button
-    //                                     text="new theme"
-    //                                     onClick={onOpen}
-    //                                     variant="secondary"
-    //                                     className="text-sm font-normal first-letter:capitalize"
-    //                                 />
-
-    //                                 <div className="flex-col">
-    //                                     {THEMES?.map((theme, index: number) => {
-    //                                         return (
-    //                                             <div key={index}>
-    //                                                 <ThemeCard
-    //                                                     activeTheme={
-    //                                                         index ===
-    //                                                         selectedThemeIndex
-    //                                                     }
-    //                                                     themeData={theme}
-    //                                                     onClick={() =>
-    //                                                         onSelectedTheme(
-    //                                                             index as number,
-    //                                                         )
-    //                                                     }
-    //                                                     toggleNewThemeModal={
-    //                                                         onOpen
-    //                                                     }
-    //                                                 />
-    //                                             </div>
-    //                                         );
-    //                                     })}
-    //                                 </div>
-    //                             </div>
-    //                         )}
-    //                     </>
-    //                 ) : (
-    //                     <EditorSpinner
-    //                         startColor="#53389E"
-    //                         endColor="#d9d9d9"
-    //                         size="10"
-    //                     />
-    //                 )}
-    //             </>
-    //         )}
-    //     </div>
-    // );
 };
