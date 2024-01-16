7a351e6c0ab66bdd56ed4d8f021026c2b2c5330d
Afeez G Lawal <afeezlg@gmail.com>
2024-01-16 17:58:43 +0100

    add channel context

--- apps/frontend/src/modules/surveys/components/studio/distribute/index.tsx
+++ apps/frontend/src/modules/surveys/components/studio/distribute/index.tsx
@@ -1,3 +1,4 @@
+import { ChannelProvider } from "@/modules/surveys/contexts/ChannelProvider";
 import { CheckComplete, CheckPending } from "@/ui/icons";
 import * as Tabs from "@radix-ui/react-tabs";
 import { useState } from "react";
@@ -45,46 +46,48 @@ const tabs = [
 export default function Distribute() {
     const [currentTab, setCurrentTab] = useState(tabs[0].id as string);
     return (
-        <Tabs.Root
-            className="flex w-full gap-12 pl-10 pr-[45px]"
-            defaultValue={tabs[0].id}
-            value={currentTab}
-            onValueChange={(value) => setCurrentTab(value)}
-        >
-            <div className="w-[386px] pt-[155px]">
-                <Tabs.List className="flex max-w-[302px] flex-col gap-2 pt-[25px]">
-                    {tabs.map((tab) => (
-                        <Tabs.Trigger
-                            key={tab.id}
-                            value={tab.id}
-                            className="flex items-center gap-3 rounded-lg p-6 hover:bg-intg-bg-9 data-[state=active]:bg-intg-bg-9"
-                        >
-                            {tab.id === currentTab ? (
-                                <CheckComplete />
-                            ) : (
-                                <CheckPending />
-                            )}
-                            <span className="min-w-max text-base text-intg-text">
-                                {tab.name}
-                            </span>
-                        </Tabs.Trigger>
-                    ))}
-                </Tabs.List>
-            </div>
+        <ChannelProvider>
+            <Tabs.Root
+                className="flex w-full gap-12 pl-10 pr-[45px]"
+                defaultValue={tabs[0].id}
+                value={currentTab}
+                onValueChange={(value) => setCurrentTab(value)}
+            >
+                <div className="w-[386px] pt-[155px]">
+                    <Tabs.List className="flex max-w-[302px] flex-col gap-2 pt-[25px]">
+                        {tabs.map((tab) => (
+                            <Tabs.Trigger
+                                key={tab.id}
+                                value={tab.id}
+                                className="flex items-center gap-3 rounded-lg p-6 hover:bg-intg-bg-9 data-[state=active]:bg-intg-bg-9"
+                            >
+                                {tab.id === currentTab ? (
+                                    <CheckComplete />
+                                ) : (
+                                    <CheckPending />
+                                )}
+                                <span className="min-w-max text-base text-intg-text">
+                                    {tab.name}
+                                </span>
+                            </Tabs.Trigger>
+                        ))}
+                    </Tabs.List>
+                </div>
 
-            <>
-                {tabs.map((tab) => {
-                    return (
-                        <Tabs.Content
-                            key={tab.id}
-                            value={tab.id}
-                            className="scrollbar-hide h-screen flex-1 overflow-y-scroll pb-[27px] pt-[155px]"
-                        >
-                            {tab.content}
-                        </Tabs.Content>
-                    );
-                })}
-            </>
-        </Tabs.Root>
+                <>
+                    {tabs.map((tab) => {
+                        return (
+                            <Tabs.Content
+                                key={tab.id}
+                                value={tab.id}
+                                className="scrollbar-hide h-screen flex-1 overflow-y-scroll pb-[27px] pt-[155px]"
+                            >
+                                {tab.content}
+                            </Tabs.Content>
+                        );
+                    })}
+                </>
+            </Tabs.Root>
+        </ChannelProvider>
     );
 }
