abf6045ee569fa6acc1fdf05bfd797f668e2c5e3
kaf-lamed-beyt <belac335@gmail.com>
2024-01-25 10:27:08 +0100

    minor

--- apps/frontend/src/modules/surveys/components/studio/create/index.tsx
+++ apps/frontend/src/modules/surveys/components/studio/create/index.tsx
@@ -41,38 +41,34 @@ export default function Create({ loader, loading }: CreateScreenProps) {
             className="flex h-screen pt-[84px]"
             defaultValue={tabs[0].label}
         >
-            <div className="border border-red-600">
-                <Tabs.List className="flex h-full flex-col gap-6 border-r border-intg-bg-4 px-[18px] pt-12">
-                    {tabs.map(({ id, label, icon }, index: React.Key) => (
-                        <Tabs.Trigger
-                            value={label}
-                            key={index}
-                            className="data-[state=active]:rounded-md data-[state=active]:bg-[#272138]"
-                            asChild
-                        >
-                            <div>
-                                <Tooltip.Provider>
-                                    <Tooltip.Root key={id}>
-                                        <Tooltip.Trigger className="h-9 rounded px-2 ease-in-out hover:bg-[#272138] hover:transition-all">
-                                            {icon}
-                                        </Tooltip.Trigger>
+            <Tabs.List className="flex h-full flex-col gap-6 border-r border-intg-bg-4 px-[18px] pt-12">
+                {tabs.map(({ id, label, icon }, index: React.Key) => (
+                    <Tabs.Trigger
+                        value={label}
+                        key={index}
+                        className="data-[state=active]:rounded-md data-[state=active]:bg-[#272138]"
+                        asChild
+                    >
+                        <div>
+                            <Tooltip.Provider>
+                                <Tooltip.Root key={id}>
+                                    <Tooltip.Trigger className="h-9 rounded px-2 ease-in-out hover:bg-[#272138] hover:transition-all">
+                                        {icon}
+                                    </Tooltip.Trigger>
 
-                                        <Tooltip.Portal>
-                                            <Tooltip.Content className="tooltip__arrow">
-                                                <div className="absolute -left-6 top-1 ml-16 rounded-md border border-intg-text-4 bg-intg-bg-9 px-1 py-2 text-xs text-intg-text-4">
-                                                    <p className="w-28">
-                                                        {label}
-                                                    </p>
-                                                </div>
-                                            </Tooltip.Content>
-                                        </Tooltip.Portal>
-                                    </Tooltip.Root>
-                                </Tooltip.Provider>
-                            </div>
-                        </Tabs.Trigger>
-                    ))}
-                </Tabs.List>
-            </div>
+                                    <Tooltip.Portal>
+                                        <Tooltip.Content className="tooltip__arrow">
+                                            <div className="absolute -left-6 top-1 ml-16 rounded-md border border-intg-text-4 bg-intg-bg-9 px-1 py-2 text-xs text-intg-text-4">
+                                                <p className="w-28">{label}</p>
+                                            </div>
+                                        </Tooltip.Content>
+                                    </Tooltip.Portal>
+                                </Tooltip.Root>
+                            </Tooltip.Provider>
+                        </div>
+                    </Tabs.Trigger>
+                ))}
+            </Tabs.List>
 
             {!loading ? (
                 <div className="flex max-h-[calc(100vh-110px)] flex-1 gap-[38px] pl-5 pr-12 pt-6">
