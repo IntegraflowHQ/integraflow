524af8d0c8131755ca126dfce680f6204e3c1880
Afeez G Lawal <afeezlg@gmail.com>
2024-02-29 14:27:45 +0100

    fix cache persist

--- apps/frontend/src/modules/apollo/hooks/useApolloFactory.ts
+++ apps/frontend/src/modules/apollo/hooks/useApolloFactory.ts
@@ -1,10 +1,10 @@
 import { InMemoryCache, NormalizedCacheObject } from "@apollo/client";
-import { useEffect, useMemo, useRef } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 import { useUpdateEffect } from "@/hooks";
 
 import { useAuth } from "@/modules/auth/hooks/useAuth";
-import { LocalForageWrapper, persistCache } from "apollo3-cache-persist";
+import { CachePersistor, LocalForageWrapper } from "apollo3-cache-persist";
 import * as localForage from "localforage";
 
 import { ApolloFactory } from "../services/apollo.factory";
@@ -12,32 +12,37 @@ import { ApolloFactory } from "../services/apollo.factory";
 const isDebugMode = import.meta.env.MODE === "development";
 
 export const useApolloFactory = () => {
+    const [persisting, setPersisting] = useState(true);
     const apolloRef = useRef<ApolloFactory<NormalizedCacheObject> | null>(null);
     const cacheRef = useRef<InMemoryCache | null>(null);
 
     const { token, currentProjectId, refresh, refreshToken, logout } = useAuth();
 
     useEffect(() => {
-        if (!cacheRef.current) {
+        async function init() {
             const cache = new InMemoryCache();
-            persistCache({
+            const persistor = new CachePersistor({
                 cache,
                 storage: new LocalForageWrapper(localForage),
-            })
-                .then(() => {
-                    cacheRef.current = cache;
-                })
-                .catch((error) => {
-                    console.error("Error persisting cache", error);
-                    cacheRef.current = cache;
-                });
+                debug: isDebugMode,
+                trigger: "write",
+            });
+            await persistor.restore();
+            cacheRef.current = cache;
+            setPersisting(false);
         }
+
+        init().finally(() => setPersisting(false));
     }, []);
 
     const apolloClient = useMemo(() => {
+        if (persisting) {
+            return null;
+        }
+
         apolloRef.current = new ApolloFactory({
             uri: `${import.meta.env.VITE_SERVER_BASE_URL}/graphql`,
-            cache: cacheRef.current ? cacheRef.current : new InMemoryCache(),
+            cache: cacheRef.current ?? new InMemoryCache(),
             defaultOptions: {
                 query: {
                     fetchPolicy: "cache-first",
@@ -57,7 +62,7 @@ export const useApolloFactory = () => {
         });
 
         return apolloRef.current.getClient();
-    }, [currentProjectId, token, refreshToken, refresh, logout]);
+    }, [currentProjectId, token, refreshToken, refresh, logout, cacheRef.current, persisting]);
 
     useUpdateEffect(() => {
         if (apolloRef.current) {
