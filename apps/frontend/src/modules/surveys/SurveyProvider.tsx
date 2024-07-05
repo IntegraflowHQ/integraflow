a175ef5a3c45bdd5b230645915168d7164172aaa
afolasope <folasopeadebanjo@gmail.com>
2024-07-05 19:14:34 +0100

    update fetch policy

--- apps/frontend/src/modules/surveys/SurveyProvider.tsx
+++ apps/frontend/src/modules/surveys/SurveyProvider.tsx
@@ -67,9 +67,7 @@ export const SurveyProvider = ({ children }: SurveyProviderProp) => {
     const [updateSurveyMutation, { error }] = useSurveyUpdateMutation({});
     const [deleteSurveyMutation] = useSurveyDeleteMutation();
 
-    const [getSurvey, { data: surveyQueryResponse, loading: loadingSurvey }] = useGetSurveyLazyQuery({
-        fetchPolicy: "cache-and-network",
-    });
+    const [getSurvey, { data: surveyQueryResponse, loading: loadingSurvey }] = useGetSurveyLazyQuery();
 
     const {
         refetch,
