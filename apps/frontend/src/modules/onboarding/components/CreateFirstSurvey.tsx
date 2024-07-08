2c6cd89326ce3b03f73a21dc8810e96404350094
Afeez G Lawal <afeezlg@gmail.com>
2024-07-08 14:33:24 +0100

    update 'publish your first survey' onboarding step ui

--- apps/frontend/src/modules/onboarding/components/CreateFirstSurvey.tsx
+++ apps/frontend/src/modules/onboarding/components/CreateFirstSurvey.tsx
@@ -1,4 +1,4 @@
-import CreateSurveyButton from "@/modules/surveys/components/partials/CreateSurveyButton";
+import SurveyCreate from "@/modules/surveys/components/SurveyCreate";
 import { ROUTES } from "@/routes";
 import { createSearchParams, useNavigate, useParams } from "react-router-dom";
 import Container, { SwitchProps } from "./Container";
@@ -9,8 +9,8 @@ export default function CreateFirstSurvey({ onComplete, ...props }: SwitchProps)
     return (
         <Container title="Create a new survey" description="Get started by creating a survey." {...props}>
             <div className="flex h-[358px] w-full items-center justify-center pb-12">
-                <CreateSurveyButton
-                    onClick={() => {
+                <SurveyCreate
+                    createFn={() => {
                         navigate({
                             pathname: ROUTES.SURVEY_LIST.replace(":orgSlug", orgSlug!).replace(
                                 ":projectSlug",
@@ -19,6 +19,8 @@ export default function CreateFirstSurvey({ onComplete, ...props }: SwitchProps)
                             search: createSearchParams({ create: "1" }).toString(),
                         });
                     }}
+                    className="h-[282px] w-[282px] pt-8"
+                    size="sm"
                 />
             </div>
         </Container>
