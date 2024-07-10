3cf15dd58f9b3a6a08a20423883a6630707f9b02
Afeez G Lawal <afeezlg@gmail.com>
2024-07-10 20:14:24 +0100

    fix channel information text overflow

--- apps/frontend/src/modules/surveys/components/studio/analyze/components/ChannelInfo.tsx
+++ apps/frontend/src/modules/surveys/components/studio/analyze/components/ChannelInfo.tsx
@@ -1,23 +1,22 @@
 import useAnalyze from "@/modules/surveys/hooks/useAnalyze";
 import { Header } from "@/ui";
-import { Fragment } from "react/jsx-runtime";
 
 export const ChannelInfo = () => {
     const { activeResponse } = useAnalyze();
 
     return (
         <div className="flex w-96 flex-col gap-[21px] rounded bg-intg-bg-15 p-3.5">
-            <Header title="Channel information" variant="3" className="[&>*]:text-intg-text-11" />
+            <Header title="Response Details" variant="3" className="[&>*]:text-intg-text-11" />
 
-            <div className="grid grid-cols-[max-content,1fr] gap-x-3.5 gap-y-2 -tracking-[0.41px]">
+            <div className="flex flex-col gap-2 -tracking-[0.41px]">
                 {Object.entries(activeResponse?.response.userAttributes ?? {}).map(([key, val]) => (
-                    <Fragment key={key}>
-                        <strong className="w-max self-center rounded bg-intg-bg-22 px-1.5 py-1 text-xs font-normal capitalize leading-[18px] text-intg-text-13">
-                            {key.replace("_", " ")}
+                    <div className="flex items-center gap-3.5" key={key}>
+                        <strong className="min-w-max rounded bg-intg-bg-22 px-1.5 py-1 text-xs font-normal capitalize leading-[18px] text-intg-text-13">
+                            {key.replace(/_/g, " ")}
                         </strong>
 
-                        <span className="self-center text-sm font-normal text-intg-text-2">{val}</span>
-                    </Fragment>
+                        <span className="truncate text-sm font-normal text-intg-text-2">{val}</span>
+                    </div>
                 ))}
             </div>
         </div>
