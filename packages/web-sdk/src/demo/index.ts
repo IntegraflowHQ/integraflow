4690e0dad9cb906a9f8e751873f2f78f7e271144
Afeez G Lawal <afeezlg@gmail.com>
2024-07-15 22:00:17 +0100

    update dateresponse and dropdownresponse validation

--- packages/web-sdk/src/demo/index.ts
+++ packages/web-sdk/src/demo/index.ts
@@ -1,14 +1,15 @@
-import Integraflow from '../index';
-import { templates } from './templates';
+import Integraflow from "../index";
+import { templates } from "./templates";
 
 (function() {
-  const integraflow = Integraflow.init({
-    surveys: templates.map(t => t.survey),
-    debug: true,
-    onQuestionAnswered(surveyId, questionId, answers) {
-      console.log(surveyId, questionId, answers);
-    },
-  });
+    const integraflow = Integraflow.init({
+        surveys: templates.map(t => t.survey),
+        debug: true,
+        syncPolicy: "off",
+        onQuestionAnswered(surveyId, questionId, answers) {
+            console.log(surveyId, questionId, answers);
+        }
+    });
 
-  integraflow.showSurvey(1);
+    integraflow.showSurvey(1);
 })();
