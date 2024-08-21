fc0e9c01aced4bb278b483e29cf09b3d7b22f1a6
afolasope <folasopeadebanjo@gmail.com>
2024-08-21 14:26:30 +0100

    update workspace test

--- apps/frontend/tests/workspace/create-workspace.spec.ts
+++ apps/frontend/tests/workspace/create-workspace.spec.ts
@@ -12,24 +12,9 @@ test.describe("Create workspace", () => {
         await page.waitForSelector("h3");
         await expect(page.locator("h3")).toContainText("Create a new workspace");
     });
-    // test.use({ storageState: NON_ONBOARDED_USER_FILE });
 
     test("should allow user to create a new workspace", async ({ page }) => {
         await page.locator('[name="workspaceName"]').fill(faker.word.words(2));
-        // await page.locator('[name="workspaceUrl"]').fill(faker.word.words(2));
-        await page.getByRole("button", { name: /Create Workspace/i }).click();
-
-        await page.waitForURL((url) => {
-            return ROUTES.SURVEY_LIST_URL.test(url.pathname);
-        });
-
-        await expect(page.locator("h1").nth(0)).toContainText(/Create your first survey/i);
-    });
-
-    test("should allow specifying workspace URL", async ({ page }) => {
-        await page.locator('[name="workspaceName"]').fill(faker.word.words(2));
-        await page.locator('[name="workspaceUrl"]').fill(workspaceUrl);
-
         await page.getByRole("button", { name: /Create Workspace/i }).click();
 
         await page.waitForURL((url) => {
@@ -40,6 +25,19 @@ test.describe("Create workspace", () => {
     });
 
     test("should not allow repeating workspace URL", async ({ page }) => {
+        await page.locator('[name="workspaceName"]').fill(faker.word.words(2));
+        await page.locator('[name="workspaceUrl"]').fill(workspaceUrl);
+
+        await page.getByRole("button", { name: /Create Workspace/i }).click();
+
+        await page.waitForURL((url) => {
+            return ROUTES.SURVEY_LIST_URL.test(url.pathname);
+        });
+
+        await expect(page.locator("h1").nth(0)).toContainText(/Create your first survey/i);
+
+        await page.goto(ROUTES.CREATE_WORKSPACE_URL);
+        await page.waitForSelector("h3");
         await page.locator('[name="workspaceName"]').fill(faker.word.words(2));
         await page.locator('[name="workspaceUrl"]').fill(workspaceUrl);
         await page.getByRole("button", { name: /Create Workspace/i }).click();
